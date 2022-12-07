871c48a2b5ab5fc0e6e615c2abce499f27dd8647
Usama-10 <usamaisthiaq768@gmail.com>
2022-12-07 07:41:17 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,21 @@
+var myVar;
+      var timer = document.getElementById("userinput");
+      var countDownSeconds;
+      function startTime(){ 
+        myVar = setInterval(start, 1000);
+        document.getElementById("timerr").innerHTML = timer.value;
+        countDownSeconds = timer.value;
+      } 
+
+      function start(){
+        countDownSeconds--;
+        document.getElementById("timerr").innerHTML = countDownSeconds;
+        if (countDownSeconds == -1){
+          stop();
+          document.getElementById("timerr").innerHTML = "0";  
+        }
+      }
+
+      function stop(){
+        clearInterval(myVar);
+      }
